9db429142f46f2efebebddcd92ba3500d666af73
sekkeiya <sekkeiyanosagyoubeya@gmail.com>
2026-03-04 15:57:47 +0900

    リファクタリング＆ログイン＆サインアップ改修

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// ✅ 過去の Service Worker が残っている場合は強制解除（白画面キャッシュ対策）
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let r of registrations) r.unregister();
+  });
+}
+
 import App from "@/app/App";
 import './index.css'
 
